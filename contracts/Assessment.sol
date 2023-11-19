3a7b9ab25df5b3c21a8e0861df329e7caf20e5fc
Kermit Sudoku <202010985@fit.edu.ph>
2023-11-19 18:53:09 +0000

    Updating files for requirements

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.9;
 //import "hardhat/console.sol";
 
 contract Assessment {
-    address payable public owner;
-    uint256 public balance;
 
+	address payable public owner;
+    uint256 public balance;
+	
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
 
@@ -14,12 +15,15 @@ contract Assessment {
         owner = payable(msg.sender);
         balance = initBalance;
     }
+	
+	receive() external payable {    
+    }
 
     function getBalance() public view returns(uint256){
         return balance;
     }
-
-    function deposit(uint256 _amount) public payable {
+	
+	function deposit(uint256 _amount) public payable {
         uint _previousBalance = balance;
 
         // make sure this is the owner
@@ -34,8 +38,8 @@ contract Assessment {
         // emit the event
         emit Deposit(_amount);
     }
-
-    // custom error
+	
+	// custom error
     error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
 
     function withdraw(uint256 _withdrawAmount) public {
@@ -57,4 +61,30 @@ contract Assessment {
         // emit the event
         emit Withdraw(_withdrawAmount);
     }
+	
+	function addVals(int256 num1, int256 num2) public pure returns (int256) {
+
+        int256 result = num1 + num2; 
+        return result; 
+	}
+	
+	function subVals(int256 num1, int256 num2) public pure returns (int256) {
+
+        int256 result = num1 - num2; 
+        return result; 
+	}
+	
+	function mulVals(int256 num1, int256 num2) public pure returns (int256) {
+        
+        int256 result = num1 * num2; 
+        return result; 
+	}
+	
+	function divVals(int256 num1, int256 num2) public pure returns (int256) {
+
+        int256 result = num1 / num2; 
+        return result; 
+	}
 }
+
+
--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,89 @@ import {ethers} from "ethers";
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
+	
   const [ethWallet, setEthWallet] = useState(undefined);
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [formType, setFormType] = useState(null);
+  const [num1, setNum1] = useState("");
+  const [num2, setNum2] = useState("");
+  const [result, setResult] = useState("");
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
+  
+  const alignment = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
+
+  const styles = {
+    main: {
+      margin: 0,
+      padding: '20px',
+      width: '100',
+      textAlign: 'center',
+      ...alignment,
+      fontFamily: 'Lucida Console, Arial, sans-serif'
+    },
+    header: {
+      margin: 0,
+      color: "#C0C0C0",
+      fontSize: 25
+    },
+    bodyContainer:{
+      width: '100%',
+      ...alignment,
+      gap: 30
+
+    },
+    body: {
+      backgroundColor: "#20B2AA",
+      width: '50%',
+      ...alignment,
+      padding: 20,
+      color: "#FFFFFF",
+      fontSize: 20,
+      borderRadius: 10
+    },
+    buttonContainer: {
+      display: 'flex',      
+      alignContent: 'space-between',
+      gap: 15
+    },
+    buttonPrimary: {
+      backgroundColor: "#C0C0C0",
+      fontFamily: 'Lucida Console, Arial, sans-serif',
+      padding: 10,
+      fontSize: 15,
+      fontWeight: 'bold'
+    },
+    buttonSecondary: {
+      backgroundColor: "#C0C0C0",
+      fontFamily: 'Lucida Console, Arial, sans-serif',
+      padding: 10,
+    },
+    form: {
+      backgroundColor: "#20B2AA",
+      width: '100%',
+      padding: 10,
+      borderRadius: 10,
+      color: "#FFFFFF",
+      ...alignment,
+      alignContent: 'space-between',
+      gap: 10,
+      textAlign: 'center'
+    },
+    subtext: {
+      ...alignment,
+      fontFamily: 'Lucida Console, Arial, sans-serif',
+      fontSize: 20
+    }
+  };
 
   const getWallet = async() => {
     if (window.ethereum) {
@@ -23,11 +99,11 @@ export default function HomePage() {
   }
 
   const handleAccount = (account) => {
-    if (account) {
-      console.log ("Account connected: ", account);
-      setAccount(account);
-    }
-    else {
+    if (account && account.length > 0) {
+      const activeAccount = account[0];
+      console.log("Account connected: ", activeAccount);
+      setAccount(activeAccount);
+    } else {
       console.log("No account found");
     }
   }
@@ -61,20 +137,82 @@ export default function HomePage() {
 
   const deposit = async() => {
     if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
+      try{
+        let tx = await atm.deposit(1);
+        await tx.wait()
+        getBalance();
+      } catch (error){
+          alert("Deposit unsuccessful!");
+      }
+
     }
   }
 
   const withdraw = async() => {
+    if (atm) {
+      try{
+        let tx = await atm.withdraw(1);
+        await tx.wait()
+        getBalance();
+      } catch (error){
+        alert("Withdraw unsuccessful!")
+      }
+
+    }
+  }
+
+  const wwwithdraw = async() => {
     if (atm) {
       let tx = await atm.withdraw(1);
       await tx.wait()
       getBalance();
     }
   }
-
+  
+  const addVals = async() => {
+    if (atm){ 
+      try {
+        setResult((await atm.addVals(num1,num2)).toNumber());
+        
+      } catch (error){
+        alert("Error calculating sum:", error.message);
+      }
+    }
+  }
+  
+  const subVals = async() => {
+	if (atm){ 
+      try{
+          setResult((await atm.subVals(num1,num2)).toNumber());
+        
+      } catch (error){
+        alert("Error calculating difference:", error.message);
+      }
+    }
+  }
+  
+  const mulVals = async() => {
+	if (atm){ 
+      try{
+          setResult((await atm.mulVals(num1,num2)).toNumber());
+        
+      } catch (error){
+        alert("Error calculating product:", error.message);
+      }
+    }
+  }
+  
+  const divVals = async() => {
+	if (atm){ 
+      try{
+          setResult((await atm.divVals(num1,num2)).toNumber());
+        
+      } catch (error){
+        alert("Error calculating quotient:", error.message);
+      }
+    }
+  }
+  
   const initUser = () => {
     // Check to see if user has Metamask
     if (!ethWallet) {
@@ -83,7 +221,7 @@ export default function HomePage() {
 
     // Check to see if user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return <button style={styles.buttonPrimary} onClick={connectAccount}>Please connect your Metamask wallet</button>
     }
 
     if (balance == undefined) {
@@ -91,27 +229,116 @@ export default function HomePage() {
     }
 
     return (
-      <div>
-        <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+      <div style={styles.bodyContainer}>
+        <div style={styles.body}>
+          <p>Your Account: {account}</p>
+          <p>Your Balance: {balance}</p>
+          <p>Your Numbers: {num1} and {num2}</p>
+          <p>Your Integer Result: {result}</p>
+        </div>
+        <div style={styles.buttonContainer}>
+          <button style={styles.buttonPrimary} onClick={withdraw}>Withdraw 1 ETH</button>
+          <button style={styles.buttonPrimary} onClick={deposit}>Deposit 1 ETH</button>
+          <button style={styles.buttonPrimary} onClick={() => setFormType("add")}>Add 2 Numbers</button>
+			    <button style={styles.buttonPrimary} onClick={() => setFormType("sub")}>Subtract 2 Numbers</button>
+			    <button style={styles.buttonPrimary} onClick={() => setFormType("mul")}>Multiply 2 Numbers</button>
+			    <button style={styles.buttonPrimary} onClick={() => setFormType("div")}>Divide 2 Numbers</button>
+        </div>
+
+        <div className="form">
+          {renderForm()}
+        </div>
       </div>
-    )
+    );
   }
 
+  const renderForm = () => {
+    const formStyle = {
+
+    };
+    switch (formType) {
+      case "add":
+        return (
+          <div style={styles.form}>
+            <p>Enter 2 numbers: </p>
+              <input
+                type="number"
+                value={num1}
+                onChange={(e) => setNum1(e.target.value)}
+              />
+			        <input
+                type="number"
+                value={num2}
+                onChange={(e) => setNum2(e.target.value)}
+              />
+            <button style={styles.buttonSecondary} onClick={addVals} >Add</button>
+
+          </div>
+        );
+	  case "sub":
+        return (
+          <div style={styles.form}>
+            <p>Enter 2 numbers: </p>
+              <input
+                type="number"
+                value={num1}
+                onChange={(e) => setNum1(e.target.value)}
+              />
+			        <input
+                type="number"
+                value={num2}
+                onChange={(e) => setNum2(e.target.value)}
+              />
+            <button style={styles.buttonSecondary} onClick={subVals} >Subtract</button>
+
+          </div>
+        );
+	  case "mul":
+        return (
+          <div style={styles.form}>
+            <p>Enter 2 numbers: </p>
+              <input
+                type="number"
+                value={num1}
+                onChange={(e) => setNum1(e.target.value)}
+              />
+			        <input
+                type="number"
+                value={num2}
+                onChange={(e) => setNum2(e.target.value)}
+              />
+            <button style={styles.buttonSecondary} onClick={mulVals} >Multiply</button>
+
+          </div>
+        );
+	  case "div":
+        return (
+          <div style={styles.form}>
+            <p>Enter 2 numbers: </p>
+              <input
+                type="number"
+                value={num1}
+                onChange={(e) => setNum1(e.target.value)}
+              />
+			        <input
+                type="number"
+                value={num2}
+                onChange={(e) => setNum2(e.target.value)}
+              />
+            <button style={styles.buttonSecondary} onClick={divVals} >Divide</button>
+
+          </div>
+        );
+    }
+  };
+
   useEffect(() => {getWallet();}, []);
+  useEffect(() => {document.body.style.backgroundColor = "#2C2F33"})
 
   return (
-    <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+    <main style={styles.main}>
+      <header style={styles.header}><h1>Arithmetic ATM</h1></header>
       {initUser()}
-      <style jsx>{`
-        .container {
-          text-align: center
-        }
-      `}
-      </style>
     </main>
   )
 }
